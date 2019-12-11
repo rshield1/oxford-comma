2969db37f0a650b335a8c5b0463cfbb21cfe4129
Robert Shields <robert.shieldsiv@gmail.com>
2019-12-11 04:22:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
 case array
   when array.length == 1
     array.join(" and ")
-  when array.length == 3
+  when array.length == 2
     array.insert(2, " ,and ")
   else
     array.join(" and ")
