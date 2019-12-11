967de45d48e36e974ee3439d9bdac70629835c08
Robert Shields <robert.shieldsiv@gmail.com>
2019-12-11 05:55:55 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  if array.length == 1
+      return array.join
+  elsif array.length == 2
+    array = array.join(' and ')
+    return array
+  elsif array.length == 3
+    array = array.insert(-2, "and").join(", ")
+    array.sub! 'and,', 'and'
+    return array
+  else  
+    array = array.insert(-2, "and").join(", ")
+    array.sub! 'and,', 'and'
+    return array
+  end
 end
+
+
+
