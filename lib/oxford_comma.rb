c140d29457c041ca1ebbaf9ce16f737f2bf452ff
Robert Shields <robert.shieldsiv@gmail.com>
2019-12-11 05:14:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,12 @@
 def oxford_comma(array)
-  case array.length
-  when array.length == 1
-    array.join(' and ')
- end
- puts array
+  if array.length == 1
+      puts array
+  elsif array.length == 2
+    array = array.join(' and ')
+    puts array
+  elsif array.length == 3
+    array = array.insert(-2, "and").join(" ")
+    puts array
+  else puts "whoa there" 
+  end
 end
