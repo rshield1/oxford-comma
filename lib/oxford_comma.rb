adb5bb6942733ec98cd8639a1f0f9a2741a60963
Robert Shields <robert.shieldsiv@gmail.com>
2019-12-11 04:08:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,10 +2,10 @@ def oxford_comma(array)
   
 case array.join
   when array.length == 2
-    array.join[1]" and ")
+    array.join[1](" and ")
   when array.length == 3
-    array.join[2](" and ")
+    array.join(" and ")
   else
-    array.join[-1](" and ")
+    array.join(" and ")
 end
 
