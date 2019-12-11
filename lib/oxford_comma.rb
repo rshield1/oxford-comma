31a4451fbb7015e185efc25f231d204a5c079ac2
Robert Shields <robert.shieldsiv@gmail.com>
2019-12-11 05:20:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
     array = array.join(' and ')
     return array
   elsif array.length == 3
-    array = array.insert(-2, "and").join(", ")
+    array = array.insert(-2, "and").join(" ")
     return array
   else puts "whoa there" 
   end
