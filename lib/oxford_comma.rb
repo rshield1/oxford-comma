8f37ee3b29a2b5e7cd7079dfcd1a3ad15a4d7353
Robert Shields <robert.shieldsiv@gmail.com>
2019-12-11 04:07:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,11 @@
 def oxford_comma(array)
-array.join(" and ")
-end
-
-
-
+  
 case array.join
   when array.length == 2
-    array.join(" and ")
+    array.join[1]" and ")
   when array.length == 3
+    array.join[2](" and ")
+  else
+    array.join[-1](" and ")
+end
+
